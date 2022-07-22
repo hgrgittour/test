7ec400d6b45c62d61bcf6ec35d11556be8245372
Guanru He <guanru_he@epam.com>
2022-07-22 11:44:39 +0800

    change file reader to nio

--- src/main/java/test/task/FileReaderJob.java
+++ src/main/java/test/task/FileReaderJob.java
@@ -9,6 +9,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.*;
+import java.util.List;
 
 public class FileReaderJob implements Runnable {
     private String filePath;
@@ -21,49 +22,32 @@ public class FileReaderJob implements Runnable {
     public void run() {
         try {
 
-        WatchService watchService = FileSystems.getDefault().newWatchService();
-        Path path = Paths.get(filePath);
-        path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
-        while (true) {
-            WatchKey watchKey = watchService.take();
-
-            for (WatchEvent event : watchKey.pollEvents()) {
-                System.out.println(event.context() + "发生了" + event.kind() + "事件！");
-                 watchKey.pollEvents() ;
-                 event.context();
-                 event.kind();
-
-
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path path = Paths.get(filePath);
+            path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+            Integer line = 0;
+            while (true) {
+                WatchKey watchKey = watchService.take();
+                for (WatchEvent event : watchKey.pollEvents()) {
+                    watchKey.pollEvents();
+                    List<String> strings = Files.readAllLines(Paths.get(filePath + "\\" + event.context()));
+                    for (int i = line; i < strings.size(); i++) {
+                        String strTmp = strings.get(i);
+                        if (strTmp != null && strTmp.equals("quit")) {
+                            System.exit(0);
+                        }
+                        if (!StringUtil.isNullOrEmpty(strTmp)) {
+                            String[] s = strTmp.split(" ");
+                            CollectionDb.set(s[0], Integer.valueOf(s[1]));
+                            line += 1;
+                        }
+                    }
+                }
+                watchKey.reset();
             }
-
-            watchKey.reset();
-        }
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-
         }
-        //        BufferedReader buffReader = null;
-//        try {
-//            FileInputStream fin = new FileInputStream(filePath);
-//            InputStreamReader reader = new InputStreamReader(fin);
-//            buffReader = new BufferedReader(reader);
-//            String strTmp = "";
-//            while (true) {
-//                //check
-//                strTmp = buffReader.readLine();
-//                if (strTmp != null && strTmp.equals("quit")) {
-//                    System.exit(0);
-//                }
-//                if (!StringUtil.isNullOrEmpty(strTmp)) {
-//                    String[] s = strTmp.split(" ");
-//                    CollectionDb.set(s[0], Integer.valueOf(s[1]));
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            System.exit(0);
-//        }
     }
 
     //    @Override
